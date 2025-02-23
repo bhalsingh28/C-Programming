6bde3f2ce1fd56cab4be0149bfe59371b7aac526
Ken <sibhal34@gmail.com>
2025-02-23 17:25:44 +0530

    Bubble Sort

--- Week 1/Sorting.cpp
+++ Week 1/Sorting.cpp
@@ -2,8 +2,14 @@
 #include <iostream>
 using namespace std;
 
-void BubbleSort(vector<int> vec) {
-
+void BubbleSort(vector<int>& vec) {
+    for(int i=0;i<vec.size()-1;i++) {
+        for(int j=0;j<vec.size()-1;j++) {
+            if(vec[j]>vec[j+1]) {
+                swap(vec[j], vec[j+1]);
+            }
+        }
+    }
 };
 
 void InsertionSort(vector<int> vec) {
@@ -23,13 +29,13 @@ void SelectionSort(vector<int> vec) {
 };
 
 int main() {
-    vector <int> vec1 = {1,6,9,25,7,0,2};
+    vector <int> vec1 = {1, 2,4,5,5,8,9,64,3,2,1,4,24,24,5,5,6,7,8,9, 2, 3};
     vector <int> vec2 = {5,7,2,6,0,6,3,8};
 
     // BUBBLE SORT : Choose element one by one and take them to their position
     // TIME COMPLEXITY : O(n^2)
     BubbleSort(vec1);
-    BubbleSort(vec1);
+    BubbleSort(vec2);
 
 
 
@@ -37,7 +43,5 @@ int main() {
     for(auto i:vec1) cout << i << "  ";
     cout << endl;
     for(auto i:vec2) cout << i << "  ";
- 
-
     return 0;
 }
