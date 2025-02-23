910e25419e0842b1077b939bc0631c665fe93cbc
Ken <sibhal34@gmail.com>
2025-02-23 17:07:52 +0530

    Lets see its empty or not

---
+++ Week 1/Sorting.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void BubbleSort(vector<int> vec) {
+
+};
+
+void InsertionSort() {
+
+};
+
+void MergeSort() {
+
+};
+
+void QuickSort() {
+
+};
+
+void SelectionSort() {
+
+};
+
+int main() {
+    vector <int> vec1 = {1,6,9,25,7,0,2};
+    vector <int> vec2 = {5,7,2,6,0,6,3,8};
+
+    // BUBBLE SORT : Choose element one by one and take them to their position
+    // TIME COMPLEXITY : O(n^2)
+    BubbleSort(vec1);
+    BubbleSort(vec1);
+
+
+
+
+    for(auto i:vec1) cout << i << "  ";
+    cout << endl;
+    for(auto i:vec2) cout << i << "  ";
+
+
+    return 0;
+}
